675f12288055d5426b27b7ce67b0807af1b1fcc0
LeoStafu <31892752+LeoStafu@users.noreply.github.com>
2018-10-05 12:37:29 +0200

    Add filtering of algniment down to 140k results

--- candidates_homologous_pairs.R
+++ candidates_homologous_pairs.R
@@ -1,12 +1,20 @@
+library(tidyverse)
+
+#Parameters
+filename_IN = file.choose()
+filter_pcID = 40
+filter_alignLength = 100
+
+
 #IMPORTING DATA
-setwd("m2_saclay/comparative_genomics/theobroma_genomics/")
-  blastp_out = read.table(
-    file = "./results_BLASTp.tsv",
-    header = F)
-  colnames(blastp_out) = c(
+blastp_out = read.table(
+    file = filename_IN,
+    header = F, stringsAsFactors = F, 
+    comment.char = "#")
+colnames(blastp_out) = c(
     "query",
     "subject",
-    "%id",
+    "pc_id",
     "alignment_length",
     "mistmatches",
     "gap_openings",
@@ -17,12 +25,36 @@ setwd("m2_saclay/comparative_genomics/theobroma_genomics/")
     "e-value",
     "bit_score"
   )
+
+blastp_out = as.tbl(blastp_out)
+
+# First filtering based on length and id % and get rid of autoblast
+blastp_out = blastp_out %>% filter(query != subject)
+blastp_out = blastp_out %>% filter(pc_id >= filter_pcID,
+                                   alignment_length >= filter_alignLength )
+
+
+# Second filtering based on couples of query - subject
+
+blastp_out$couple_name = apply(blastp_out, 1, function(x){
+                            ids = sort(c(x[1], x[2]))
+                            paste(ids, collapse = "-")
+                        })
+
+blastp_out %>% group_by(couple_name) %>% slice(which.max(pc_id)) %>% ungroup()
+
+
+
+#SORTING
+blastp_out = blastp_out[order(blastp_out$bit_score, 
+                      decreasing = TRUE),
+                ]
 #FILTERING
     blastp_out = subset(x = blastp_out, query != subject)
     blastp_out = subset(x = blastp_out, alignment_length >= 100)
     blastp_out = subset(x = blastp_out, `%id` >= 75)
+    summary(blastp_out)
 #EXPORTING
     write.table(blastp_out,
                 file = "blastp_filtered",
                 sep = '\t')
-    
