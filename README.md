e11fcfb7ac65fe1ace492ffaa3e83d90a33a229f
tbersez <thomas.bersez@gmail.com>
2018-11-25 17:56:05 +0100

    clean

--- README.md
+++
@@ -1,9 +0,0 @@
-# Duplicated genes and genes family in Theobroma cacao genome, case study of the Cysteine-rich kinases superfamily
-## CRK8 protein family case study
-*Master genomics projet, Paris Saclay University*
-
-## Abstract
-
-## Link to the full report
-
-
--- circos_generator.R
+++
@@ -1,7 +0,0 @@
-if(!require(circlize)){
-    install.packages("circlize")
-}
-library(circlize)
-chr_data = read.table(file = "chromInfo_Tcacao.txt", stringsAsFactors = F)
-chr_data = read.chromInfo(chromInfo = chr_data)
-circos.genomicInitialize(data = chr_data$df)
