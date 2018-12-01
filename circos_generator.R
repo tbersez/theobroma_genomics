d60124a4ce73c04e214656eb5e083f52e6f1108b
tbersez <thomas.bersez@gmail.com>
2018-12-01 15:25:13 +0100

    still not working :(

--- circos_generator.R
+++ circos_generator.R
@@ -2,6 +2,6 @@ if(!require(circlize)){
     install.packages("circlize")
 }
 library(circlize)
-chr_data = read.table(file = "chromInfo_Tcacao.txt", stringsAsFactors = F)
+chr_data = read.table(file = file.choose(), stringsAsFactors = F)
 chr_data = read.chromInfo(chromInfo = chr_data)
 circos.genomicInitialize(data = chr_data$df)
